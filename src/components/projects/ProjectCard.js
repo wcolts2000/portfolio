737cb62621792bf1e450755ed4d4c57d6f126cca
wcolts2000 <wcolts2000@gmail.com>
2020-01-17 12:03:43 -0800

    pulled project card out to its own component

---
+++ src/components/projects/ProjectCard.js
@@ -0,0 +1,204 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+// ==============================
+// ========= COMPONENT ==========
+// ==============================
+
+function ProjectCard({url, titleTop, titleBottom, subtitle, techUsed, backTitle, backBody, projectCode, projectLive}) {
+  return (
+    <Card className="card">
+      <div className="card__content">
+        <Front
+          url={require('../../assets/' + url)}
+          className="card__front"
+        >
+          <div className="card__title">
+            {titleTop} <br />
+            {titleBottom}
+          </div>
+  <div className="card__subtitle">{subtitle}</div>
+          <div className="techstack">
+            <h3>Tech Used</h3>
+            <ul>
+              {techUsed.map((tech, i) => (
+                <li key={i}>{tech}</li>
+              ))}
+            </ul>
+          </div>
+        </Front>
+        <div className="card__back">
+              <h3>{backTitle}</h3>
+          <p className="card__body">
+          {backBody}
+          </p>
+          <div className="icon-container">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href={projectCode}
+            >
+              <FontAwesomeIcon className="icon" icon={['fab', 'github']} /> Code
+            </a>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href={projectLive}
+            >
+              <FontAwesomeIcon className="icon" icon={['fas', 'desktop']} /> Live
+            </a>
+          </div>
+        </div>
+      </div>
+    </Card>
+  )
+}
+
+export default ProjectCard
+
+// ==============================
+// ===========  STYLES  =========
+// ==============================
+
+const Card = styled.div`
+    width: 270.5px;
+    perspective: 800px;
+    height: 420px;
+    margin: 7rem 0;
+
+  .card__content {
+    height: 420px;
+    text-align: center;
+    position: relative;
+    padding: 2rem;
+    transition: transform 1s;
+    transform-style: preserve-3d;
+    text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.8);
+  }
+
+  :hover .card__content {
+    transform: rotateY(0.5turn);
+  }
+
+  
+  .card__front,
+  .card__back {
+    color: white;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    backface-visibility: hidden;
+    transform-style: preserve-3d;
+    display: grid;
+    align-content: center;
+    box-shadow: 2px 2px 7px rgba(0, 0, 0, 0.2);
+  }
+  .card__front .techstack {
+    order: 3;
+    h3 {
+      font-size: 2.4rem;
+      padding: 0 2rem;
+    }
+
+    ul {
+      list-style-type: square;
+      font-size: 2rem;
+      list-style: inside;
+      text-align: left;
+    }
+  }
+
+  .card__front::before {
+    content: '';
+    position: absolute;
+    --spacer: 2rem;
+    top: var(--spacer);
+    bottom: var(--spacer);
+    left: var(--spacer);
+    right: var(--spacer);
+    border: 3px solid white;
+    transform: translateZ(3rem);
+  }
+
+  .card__title {
+    font-size: 3.5rem;
+    transform: translateZ(9rem);
+    order: 2;
+    text-transform: uppercase;
+    margin: 1rem 0 2rem;
+  }
+
+  .card__subtitle {
+    transform: translateZ(6rem);
+    text-transform: uppercase;
+    letter-spacing: 4px;
+    font-size: 1rem;
+    font-weight: bold;
+  }
+
+  .card__body {
+    transform: translateZ(6rem);
+    font-weight: normal;
+    font-size: 1.5rem;
+    line-height: 2;
+  }
+  .card__back {
+    transform: rotateY(0.5turn);
+    color: white;
+    background: #26235a;
+    padding: 2rem 2.6rem;
+
+    h3 {
+      font-size: 2rem;
+      line-height: 1.5;
+      transform: translateZ(3rem);
+    }
+
+    .icon-container {
+      width: 100%;
+      padding: 1rem;
+      justify-content: space-around;
+
+      a {
+        display: flex;
+        color: white;
+        font-size: 2rem;
+        line-height: initial;
+        letter-spacing: 2.5px;
+        font-weight: bold;
+        margin: 1rem 0;
+        text-transform: uppercase;
+        transition: all 0.3s ease-in-out;
+        text-decoration: none;
+        padding: 1rem 2rem;
+        border-radius: 50px;
+        border: 2px solid white;
+        
+        :hover {
+          border: 2px solid white;
+          background: white;
+          color: #26235a;
+        }
+      }
+
+      .icon {
+        transform: translateZ(4rem);
+        font-size: 2rem;
+        margin: 0 1.6rem;
+
+      }
+    }
+  }
+`;
+
+const Front = styled.div`
+  background-color: #6c63ff;
+  background-size: cover;
+  background-position: top;
+  background-blend-mode: multiply;
+  color: #333;
+  background-image: linear-gradient(rgba(0,0,0,.3), rgba(0,0,0,.3)), url(${props => props.url});
+`;
--- src/components/projects/ProjectsSection.js
+++ src/components/projects/ProjectsSection.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Card from "./ProjectCard"
 
 // =================================
 // ===========  COMPONENT  =========
@@ -11,141 +12,39 @@ function ProjectsSection() {
     <Section id="projects">
       <h2>Projects</h2>
       <div className="container">
-        <div className="card">
-          <div className="card__content">
-            <Front
-              url={require('../../assets/connectourkids.jpg')}
-              className="card__front"
-            >
-              <div className="card__title">
-                Connect <br />
-                Our Kids
-              </div>
-              <div className="card__subtitle">A react native app</div>
-              <div className="techstack">
-                <h3>Tech Used</h3>
-                <ul>
-                  <li>React Native</li>
-                  <li>EXPO</li>
-                  <li>Native Base</li>
-                </ul>
-              </div>
-            </Front>
-            <div className="card__back">
-              <h3>Connect Our Kids: A People Search App</h3>
-              <p className="card__body">
-              Helping Social Workers find extended family members for children entering the foster care system.
-              </p>
-              <div className="icon-container">
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://github.com/labs13-connect-kids/mobile"
-                >
-                  <FontAwesomeIcon className="icon" icon={['fab', 'github']} /> Code
-                </a>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://expo.io/@connectourkids/connectOurKids?release-channel=staging"
-                >
-                  <FontAwesomeIcon className="icon" icon={['fas', 'desktop']} /> Beta
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card__content">
-            <Front
-              url={require('../../assets/hurryupprint.jpg')}
-              className="card__front"
-            >
-              <div className="card__title">
-                Hurry <br />
-                Up Print
-              </div>
-              <div className="card__subtitle">A printshop website</div>
-              <div className="techstack">
-                <h3>Tech Used</h3>
-                <ul>
-                  <li>PHP</li>
-                  <li>MySQL</li>
-                  <li>Materialize CSS</li>
-                </ul>
-              </div>
-            </Front>
-            <div className="card__back">
-              <h3>Hurry Up Print e-commerce site</h3>
-              <p className="card__body">
-                {' '}
-                An e-commerce print shop site with user login and admin dashboard features and pricing calculators.
-              </p>
-              <div className="icon-container">
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://github.com/wcolts2000/hup-client/"
-                >
-                  <FontAwesomeIcon className="icon" icon={['fab', 'github']} /> Code
-                </a>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://hurryupprint.com"
-                >
-                  <FontAwesomeIcon className="icon" icon={['fas', 'desktop']} /> live
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card__content">
-            <Front
-              url={require('../../assets/blog.jpg')}
-              className="card__front"
-            >
-              <div className="card__title">
-                GatsbyJS <br /> 
-                Blog Site
-              </div>
-              <div className="card__subtitle">Notes/Resources</div>
-              <div className="techstack">
-                <h3>Tech Used</h3>
-                <ul>
-                  <li>GatsbyJS</li>
-                  <li>Contentful CMS</li>
-                  <li>Redux</li>
-                  <li>Styled-Components</li>
-                </ul>
-              </div>
-            </Front>
-            <div className="card__back">
-              <h3>Personal Notes and Reference Lists</h3>
-              <p className="card__body">
-                {' '}
-                Useful resources, notes, a blog, and project links made with GatsbyJS.
-              </p>
-              <div className="icon-container">
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://github.com/wcolts2000/the_pretty_good_gatsy_blog"
-                >
-                  <FontAwesomeIcon className="icon" icon={['fab', 'github']} /> code
-                </a>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://pretty-good-gatsby-blog.netlify.com/"
-                >
-                  <FontAwesomeIcon className="icon" icon={['fas', 'desktop']} /> live
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Card 
+          url="hurryupprint.jpg"
+          titleTop="Connect"
+          titleBottom="Our Kids"
+          subtitle="A react native app"
+          techUsed={['React Native', 'EXPO', 'Native Base']}
+          backTitle="Connect Our Kids: A People Search App"
+          backBody="Helping Social Workers find extended family members for children entering the foster care system."
+          projectCode="https://github.com/labs13-connect-kids/mobile"
+          projectLive="https://expo.io/@connectourkids/connectOurKids?release-channel=staging"
+        />
+        <Card 
+          url="connectourkids.jpg"
+          titleTop="Hurry"
+          titleBottom="Up Print"
+          subtitle="A printshop website"
+          techUsed={['PHP', 'MySQL', 'Materialize CSS']}
+          backTitle="Hurry Up Print e-commerce site"
+          backBody="An e-commerce print shop site with user login and admin dashboard features and pricing calculators."
+          projectCode="https://github.com/wcolts2000/hup-client/"
+          projectLive="https://hurryupprint.com"
+        />
+        <Card 
+          url="blog.jpg"
+          titleTop="GatsbyJS"
+          titleBottom="Blog Site"
+          subtitle="Notes/Resources"
+          techUsed={['GatsbyJS', 'Contentful CMS', 'Redux', 'Styled-Components']}
+          backTitle="Personal Notes and Reference Lists"
+          backBody="Useful resources, notes, a blog, and project links made with GatsbyJS."
+          projectCode="https://github.com/wcolts2000/the_pretty_good_gatsy_blog"
+          projectLive="https://pretty-good-gatsby-blog.netlify.com/"
+        />
       </div>
       <p className="description">Here are a few projects you might like looking
       through. These have been built using React, React Native, and PHP. The
@@ -198,139 +97,6 @@ const Section = styled.section`
     padding-bottom: 10rem;
   }
 
-  .card {
-    width: 270.5px;
-    perspective: 800px;
-    height: 420px;
-    margin: 7rem 0;
-  }
-
-  .card__content {
-    height: 420px;
-    text-align: center;
-    position: relative;
-    padding: 2rem;
-    transition: transform 1s;
-    transform-style: preserve-3d;
-    text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.8);
-  }
-
-  .card:hover .card__content {
-    transform: rotateY(0.5turn);
-  }
-
-  
-  .card__front,
-  .card__back {
-    color: white;
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    backface-visibility: hidden;
-    transform-style: preserve-3d;
-    display: grid;
-    align-content: center;
-    box-shadow: 2px 2px 7px rgba(0, 0, 0, 0.2);
-  }
-  .card__front .techstack {
-    order: 3;
-    h3 {
-      font-size: 2.4rem;
-      padding: 0 2rem;
-    }
-
-    ul {
-      list-style-type: square;
-      font-size: 2rem;
-      list-style: inside;
-      text-align: left;
-    }
-  }
-
-  .card__front::before {
-    content: '';
-    position: absolute;
-    --spacer: 2rem;
-    top: var(--spacer);
-    bottom: var(--spacer);
-    left: var(--spacer);
-    right: var(--spacer);
-    border: 3px solid white;
-    transform: translateZ(3rem);
-  }
-
-  .card__title {
-    font-size: 3.5rem;
-    transform: translateZ(9rem);
-    order: 2;
-    text-transform: uppercase;
-    margin: 1rem 0 2rem;
-  }
-
-  .card__subtitle {
-    transform: translateZ(6rem);
-    text-transform: uppercase;
-    letter-spacing: 4px;
-    font-size: 1rem;
-    font-weight: bold;
-  }
-
-  .card__body {
-    transform: translateZ(6rem);
-    font-weight: normal;
-    font-size: 1.5rem;
-    line-height: 2;
-  }
-  .card__back {
-    transform: rotateY(0.5turn);
-    color: white;
-    background: #26235a;
-    padding: 2rem 2.6rem;
-
-    h3 {
-      font-size: 2rem;
-      line-height: 1.5;
-      transform: translateZ(3rem);
-    }
-
-    .icon-container {
-      width: 100%;
-      padding: 1rem;
-      justify-content: space-around;
-
-      a {
-        display: flex;
-        color: white;
-        font-size: 2rem;
-        line-height: initial;
-        letter-spacing: 2.5px;
-        font-weight: bold;
-        margin: 1rem 0;
-        text-transform: uppercase;
-        transition: all 0.3s ease-in-out;
-        text-decoration: none;
-        padding: 1rem 2rem;
-        border-radius: 50px;
-        border: 2px solid white;
-        
-        :hover {
-          border: 2px solid white;
-          background: white;
-          color: #26235a;
-        }
-      }
-
-      .icon {
-        transform: translateZ(4rem);
-        font-size: 2rem;
-        margin: 0 1.6rem;
-
-      }
-    }
-  }
-
   p.description {
     font-size: 2rem;
     position: absolute;
@@ -362,11 +128,4 @@ const Section = styled.section`
   }
 `;
 
-const Front = styled.div`
-  background-color: #6c63ff;
-  background-size: cover;
-  background-position: top;
-  background-blend-mode: multiply;
-  color: #333;
-  background-image: linear-gradient(rgba(0,0,0,.3), rgba(0,0,0,.3)), url(${props => props.url});
-`;
+
